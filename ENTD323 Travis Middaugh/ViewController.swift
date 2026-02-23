2799b263168cfa878c13cc88af9c23431c8fb5c7
Travis <travism.github@gmail.com>
2026-02-23 10:15:25 -0500

    Initial Commit

---
+++ ENTD323 Travis Middaugh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ENTD323 Travis Middaugh
+//
+//  Created by Travis on 2/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
