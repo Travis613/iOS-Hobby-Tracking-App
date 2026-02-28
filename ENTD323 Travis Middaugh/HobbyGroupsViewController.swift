04a19969c42a0232bd1d793942e6466cf5fb993c
Travis <travism.github@gmail.com>
2026-02-28 08:20:17 -0500

    increased the size of the text in the table view

--- ENTD323 Travis Middaugh/HobbyGroupsViewController.swift
+++ ENTD323 Travis Middaugh/HobbyGroupsViewController.swift
@@ -10,7 +10,7 @@ class HobbyGroupsViewController: UIViewController{
         return HobbyGroupTableView
     }()
     
-    var hobbyProjects = ["MTG Life Counting Website", "Learn to play the piano", "Build this app"]
+    var hobbyProjects: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,20 +83,18 @@ class HobbyGroupsViewController: UIViewController{
 }
 
 extension HobbyGroupsViewController: UITableViewDataSource, UITableViewDelegate{
-    // Returns the number of row based on the amount of rows in out hobbies array
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return hobbyProjects.count
     }
     
-    // Configures and returns each cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
         cell.textLabel?.text = hobbyProjects[indexPath.row]
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 18.0)
         cell.accessoryType = .disclosureIndicator
         return cell
     }
     
-    // Load the individual hobby project view contoller
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         navigationController?.pushViewController(HobbyProjectViewController(hobbyProject: hobbyProjects[indexPath.row]), animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
